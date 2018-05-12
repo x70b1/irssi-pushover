7ffbad81348828b86a0d8aed814ee42c971da565
x70b1 <x70b1@users.noreply.github.com>
2018-05-13 00:45:00 +0200

    use perltidy -ce -l=0 -et=4

--- pushover.pl
+++ pushover.pl
@@ -5,61 +5,62 @@ use LWP::UserAgent;
 $VERSION = "1.0";
 
 %IRSSI = (
-    authors => 'x70b1',
-    name    => 'pushover.pl',
-    description => 'Send a notification when you receive a message and are not attached to screen.',
-    url     => 'https://github.com/x70b1/irssi-pushover',
+	authors     => 'x70b1',
+	name        => 'pushover.pl',
+	description => 'Send a notification when you receive a message and are not attached to screen.',
+	url         => 'https://github.com/x70b1/irssi-pushover',
 );
 
 sub pushover {
-    my ( $title, $message ) = @_;
+	my ( $title, $message ) = @_;
 
-    my $screen = system("screen -ls | grep ".Irssi::settings_get_str('pushover_screensession')." | grep -q Detached");
+	my $screen = system( "screen -ls | grep " . Irssi::settings_get_str('pushover_screensession') . " | grep -q Detached" );
 
-    if ( !$screen ) {
-        if ( ( $lastmsg + Irssi::settings_get_int('pushover_silence') ) < time() ) {
+	if ( !$screen ) {
+		if ( ( $lastmsg + Irssi::settings_get_int('pushover_silence') ) < time() ) {
 
-            my $push = LWP::UserAgent->new()->post(
-                'https://api.pushover.net/1/messages.json', [
-                    user => Irssi::settings_get_str('pushover_usertoken'),
-                    token => Irssi::settings_get_str('pushover_apptoken'),
-                    title => $title,
-                    message => $message
-                ]
-            );
+			my $push = LWP::UserAgent->new()->post(
+				'https://api.pushover.net/1/messages.json',
+				[
+					user    => Irssi::settings_get_str('pushover_usertoken'),
+					token   => Irssi::settings_get_str('pushover_apptoken'),
+					title   => $title,
+					message => $message
+				]
+			);
 
-            if ($push->is_success) {
-                Irssi::print("Pushover message sent.");
-            } else {
-                Irssi::print("A Pushover message failed!");
-            }
+			if ( $push->is_success ) {
+				Irssi::print("Pushover message sent.");
+			} else {
+				Irssi::print("A Pushover message failed!");
+			}
 
-            $lastmsg = time();
+			$lastmsg = time();
 
-        } else {
-            $lastmsg = time();
-        }
-    }
+		} else {
+			$lastmsg = time();
+		}
+	}
 }
 
 sub push_message {
-    my ( $server, $msg, $nick, $address, $target ) = @_;
+	my ( $server, $msg, $nick, $address, $target ) = @_;
 
-    pushover( "/msg $nick", "Ping!" );
+	pushover( "/msg $nick", "Ping!" );
 }
 
 sub push_mention {
-    my ( $server, $msg, $nick, $address, $target ) = @_;
+	my ( $server, $msg, $nick, $address, $target ) = @_;
 
-    if ( index( $msg, $server->{nick}) != -1 ) {
-        pushover( "$target", "$nick: $msg" );
-    }
+	if ( index( $msg, $server->{nick} ) != -1 ) {
+		pushover( "$target", "$nick: $msg" );
+	}
 }
 
-Irssi::settings_add_int('pushover', 'pushover_silence', 60);
-Irssi::settings_add_str('pushover', 'pushover_screensession', '');
-Irssi::settings_add_str('pushover', 'pushover_apptoken', '');
-Irssi::settings_add_str('pushover', 'pushover_usertoken', '');
+Irssi::settings_add_int( 'pushover', 'pushover_silence', 60 );
+Irssi::settings_add_str( 'pushover', 'pushover_screensession', '' );
+Irssi::settings_add_str( 'pushover', 'pushover_apptoken',      '' );
+Irssi::settings_add_str( 'pushover', 'pushover_usertoken',     '' );
 
 my $lastmsg = time() - Irssi::settings_get_int('pushover_silence');
 
